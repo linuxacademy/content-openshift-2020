e83866d5b802962898ca2c9731cb3e48ca96d38e
JesseHoch <jesse.hoch@linuxacademy.com>
2020-10-14 11:06:55 -0400

    Simplified script.

--- lab2setup.sh
+++ lab2setup.sh
@@ -4,6 +4,5 @@ echo "Lab setup is starting."
 ### Setting up HTPasswd Identity Provider ###
 wget https://raw.githubusercontent.com/linuxacademy/content-openshift-2020/master/lab_htpasswd_cr.yaml
 htpasswd -c -B -b users.htpasswd admin doubletap
-oc create secret generic htpass-secret --from-file=users.htpasswd> -n openshift-config
-oc apply -f lab_htpasswd_cr.yaml
+oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
 echo "Lab setup has finished."
