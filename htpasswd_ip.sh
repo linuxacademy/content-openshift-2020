1cfe48f463fc06a7b777052980b4d6a62a47c19c
JesseHoch <jesse.hoch@linuxacademy.com>
2020-10-14 00:06:09 -0400

    Added lab files.

--- htpasswd_ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Lab setup is starting."
-### Setting up HTPasswd Identity Provider ###
-wget https://raw.githubusercontent.com/linuxacademy/content-openshift-2020/master/lab_htpasswd_cr.yaml
-htpasswd -c -B -b users.htpasswd admin doubletap
-oc create secret generic htpass-secret --from-file=users.htpasswd> -n openshift-config
-oc apply -f lab_htpasswd_cr.yaml
-echo "Lab setup has finished."
