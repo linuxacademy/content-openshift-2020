b16c1c549271274a9ed6a66ad56b7ea4b01b51ed
JesseHoch <jesse.hoch@linuxacademy.com>
2021-01-22 14:54:49 -0500

    Lab setup script 2

---
+++ lab_setup_2.sh
@@ -0,0 +1,4 @@
+
+oc new-project twinkies
+oc new-build registry.access.redhat.com/ubi8/openjdk-11~https://github.com/monodot/simple-camel-spring-boot-app  --name=tw-tracer
+oc start-build tw-tracer --follow=false
