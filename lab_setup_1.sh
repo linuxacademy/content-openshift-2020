420701c8ba90e6b477c7bfcc9af4f8c85d1aa89f
JesseHoch <jesse.hoch@linuxacademy.com>
2021-01-22 15:07:03 -0500

    Cleaned up both scripts

--- lab_setup_1.sh
+++ lab_setup_1.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+echo "Build 1 Has started."
 oc new-project twinkies
 oc new-build registry.access.redhat.com/ubi8/openjdk-11~https://github.com/monodot/simple-camel-spring-boot-app  --name=tw-tracer
--- lab_setup_2.sh
+++ lab_setup_2.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
 
+echo "Build 1 has started."
 oc new-project twinkies
 oc new-build registry.access.redhat.com/ubi8/openjdk-11~https://github.com/monodot/simple-camel-spring-boot-app  --name=tw-tracer
+
+sleep 20
+
+echo "Build 2 has started"
 oc start-build tw-tracer --follow=false
